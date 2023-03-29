e31fbf22eaab6791f5d0e088fc3ea478b7cb41b6
David Zhang <david_zhang@windowslive.com>
2023-03-29 22:40:48 +0700

    WIP programming result entry and attempting to fix button state

--- scripts/product-master.js
+++ scripts/product-master.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const deleteBtn = document.getElementById('delete-selected-row');
-    if (deleteBtn.classList.contains('ph-btn-disabled')) {
-      deleteBtn.removeAttribute('href');
-      deleteBtn.setAttribute('disabled', true);
+  const deleteBtn = document.getElementById('delete-selected-row');
+  const isDisabled = deleteBtn.getAttribute('disabled') === 'true';
+
+  if (isDisabled) {
+    deleteBtn.removeAttribute('href');
+    deleteBtn.setAttribute('disabled', true);
+    deleteBtn.classList.add('ph-btn-disabled');
+  }
+
+  // Add an event listener to the button to toggle the `ph-btn-disabled` class
+  deleteBtn.addEventListener('click', function() {
+    if (this.getAttribute('disabled') === 'true') {
+      this.classList.add('ph-btn-disabled');
+    } else {
+      this.classList.remove('ph-btn-disabled');
     }
   });
-  
+});
