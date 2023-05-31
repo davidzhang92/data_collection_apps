537bb0cf85c925596672a75e6c70a61601b0306b
David Zhang <david_zhang@windowslive.com>
2023-05-31 16:17:11 +0700

    programming entry page done

--- scripts/programming-result-entry.js
+++ scripts/programming-result-entry.js
@@ -4,5 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
       deleteBtn.removeAttribute('href');
       deleteBtn.setAttribute('disabled', true);
     }
+
+  var passButton = document.getElementById('pass-button');
+  var failButton = document.getElementById('fail-button');
+  var inputResult = document.getElementById('input-result');
+
+  passButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    inputResult.textContent = 'PASS';
+    inputResult.style.color = '#00ff2a';
   });
-  
+
+  failButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    inputResult.textContent = 'FAIL';
+    inputResult.style.color = '#ff0000';
+  });
+});
