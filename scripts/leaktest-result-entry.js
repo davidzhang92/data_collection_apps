ab882dadd9d82db94dd36671cf1b468fb7bf1934
David Zhang <david_zhang@windowslive.com>
2023-06-05 16:18:19 +0700

    leaktest done

---
+++ scripts/leaktest-result-entry.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteBtn = document.getElementById('select-button');
+    if (deleteBtn.classList.contains('ph-btn-disabled')) {
+      deleteBtn.removeAttribute('href');
+      deleteBtn.setAttribute('disabled', true);
+    }
+
+  var passButton = document.getElementById('pass-button');
+  var failButton = document.getElementById('fail-button');
+  var inputResult = document.getElementById('input-result');
+
+  passButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    inputResult.textContent = 'PASS';
+    inputResult.style.color = '#00ff2a';
+  });
+
+  failButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    inputResult.textContent = 'FAIL';
+    inputResult.style.color = '#ff0000';
+  });
+});
