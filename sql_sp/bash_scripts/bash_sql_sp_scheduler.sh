61f86844ed93beb38ead0c54586be5fb2611014d
David Zhang <david_zhang@windowslive.com>
2023-11-02 15:43:44 +0700

    fixed scheduler bash script, start working on User Master

--- sql_sp/bash_scripts/bash_sql_sp_scheduler.sh
+++ sql_sp/bash_scripts/bash_sql_sp_scheduler.sh
@@ -6,12 +6,32 @@ UpdatePartIDWithGUID="/data-storage/sfdc_apps/sql_sp/bash_scripts/UpdatePartIDWi
 # Path to your second script
 CalculateOverallThroughput="/data-storage/sfdc_apps/sql_sp/bash_scripts/CalculateOverallThroughput.sh"
 
-while true
-do
-    # Execute UpdatePartIDWithGUID
-    bash "$UpdatePartIDWithGUID"
+# Sleep duration in seconds for each script
+SLEEP_DURATION_1=5
+SLEEP_DURATION_2=1800
 
-    # Execute CalculateOverallThroughput
-    bash "$CalculateOverallThroughput"
+# Function to run a script with a sleep interval
+run_script() {
+    SCRIPT=$1
+    SLEEP_DURATION=$2
 
-done
+    while true
+    do
+        echo "Starting $SCRIPT"
+        bash "$SCRIPT"
+        echo "Finished $SCRIPT"
+
+        # Sleep for a while
+        sleep $SLEEP_DURATION
+    done
+}
+
+# Run each script in the background with its own sleep interval
+run_script "$UpdatePartIDWithGUID" $SLEEP_DURATION_1 &
+PID1=$!
+
+run_script "$CalculateOverallThroughput" $SLEEP_DURATION_2 &
+PID2=$!
+
+# Wait for both processes to complete
+wait $PID1 $PID2
