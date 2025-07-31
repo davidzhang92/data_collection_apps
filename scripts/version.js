fec7a19551f228b1a8f46f6e1c6e45ef799660f8
David Zhang <david_zhang@windowslive.com>
2025-07-31 16:08:43 +0700

    testing cicd

--- scripts/version.js
+++ scripts/version.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $(".version").html("<b>Developed By : David Zhang | Version: 1.4.0</b>");
+    $(".version").html("<b>Developed By : David Zhang | Version: 1.4.8</b>");
 });
